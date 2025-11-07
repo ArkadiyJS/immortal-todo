80057013506d82fafe01a5abcf3d9d787d49f150
ArkadiyJS <vologodskiy20@gmail.com>
2025-11-07 13:22:11 +0500

    form todo components

--- immortal-todo/src/App.jsx
+++ immortal-todo/src/App.jsx
@@ -1,29 +1,13 @@
 import { useEffect, useState } from "react";
-import nextId from "react-id-generator";
 
 import "./App.css";
 import Tasks from "./tasks";
+import FormTodo from "./components/formTodo/formTodo";
 
 function App() {
-  const [valueInput, setValueInput] = useState("");
-
   const [dataTask, setDataTask] = useState([
     { id: 12, name: "сходить в магазин", completed: true, data: "" },
   ]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const generationID = nextId();
-    const newTask = {
-      id: generationID,
-      name: valueInput,
-      completed: false,
-      data: "",
-    };
-    setDataTask([...dataTask, newTask]);
-    setValueInput("");
-  };
 
   console.log(dataTask);
 
@@ -33,24 +17,10 @@ function App() {
     <div className="App">
       <h1 className="header">TODO LIST</h1>
       <div className="content">
-        <form className="formTodo" onSubmit={handleSubmit}>
-          <input
-            maxLength="40"
-            placeholder="Название задачи"
-            type="text"
-            value={valueInput}
-            onChange={(e) => setValueInput(e.target.value)}
-          />
-          <button type="submit" disabled={valueInput === ""}>
-            добавить задачу
-          </button>
-        </form>
-
-        <div>
-          {dataTask.map((d) => (
-            <Tasks key={d.id} name={d.name} completed={d.completed} id={d.id} />
-          ))}
-        </div>
+        <FormTodo dataTask={dataTask} setDataTask={setDataTask} />
+        {dataTask.map((d) => (
+          <Tasks key={d.id} name={d.name} completed={d.completed} id={d.id} />
+        ))}
       </div>
     </div>
   );
---
+++ immortal-todo/src/components/formTodo/formTodo.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import nextId from "react-id-generator";
+
+const FormTodo = ({ setDataTask, dataTask }) => {
+  const [valueInput, setValueInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const generationID = nextId();
+    const newTask = {
+      id: generationID,
+      name: valueInput,
+      completed: false,
+      data: "",
+    };
+    setDataTask([...dataTask, newTask]);
+    setValueInput("");
+  };
+  return (
+    <form className="formTodo" onSubmit={handleSubmit}>
+      <input
+        maxLength="40"
+        placeholder="Название задачи"
+        type="text"
+        value={valueInput}
+        onChange={(e) => setValueInput(e.target.value)}
+      />
+      <button type="submit" disabled={valueInput === ""}>
+        добавить задачу
+      </button>
+    </form>
+  );
+};
+
+export default FormTodo;
